9011f425ed0588e1358a2977034b6b0210f74061
Qiqi Wang <qiqi@mit.edu>
2014-11-28 17:07:08 -0500

    Everything working in zenSlides

--- js/zenBoard.js
+++ js/zenBoard.js
@@ -5,6 +5,7 @@
     var ref = new Firebase("https://infinite-walls.firebaseio.com/pres");
     $scope.pres = {};
     $scope.slide = {};
+    $scope.slide.zoom = 0;
     $firebase(ref.child('test-pres')).$asObject().$bindTo($scope, 'pres');
  
     $scope.$watch(function () {
@@ -16,9 +17,7 @@
         }
     })
 
-    $scope.$watch(function () {
-        return $scope.slide.url;
-    }, function (url) {
+    $scope.$watch('slide.url', function (url) {
         if (url && url.indexOf('http://') != 0 && url.indexOf('https://') != 0){
             url = 'http://' + url;
         }
@@ -102,18 +101,123 @@
             }
         }
     };
+
+    var webBox = $('#webview-box');
+    var web = $('#webview');
+    var blocker = {
+        west: $('#blocker-west'),
+        east: $('#blocker-east'),
+        north: $('#blocker-south'),
+        south: $('#blocker-north')
+    };
+    $scope.$watchGroup(['slide.vSliderOutL',
+                        'slide.vSliderOutR',
+                        'slide.vSliderInL',
+                        'slide.vSliderInR',
+                        'slide.hSliderOutL',
+                        'slide.hSliderOutR',
+                        'slide.hSliderInL',
+                        'slide.hSliderInR',
+                        'slide.zoom'], function() {
+        var outerLeft = $scope.slide.hSliderOutL * 16;
+        var outerTop = $scope.slide.vSliderOutL * 12;
+        var outerWidth = ($scope.slide.hSliderOutR
+                        - $scope.slide.hSliderOutL) * 16;
+        var outerHeight = ($scope.slide.vSliderOutR
+                         - $scope.slide.vSliderOutL) * 12;
+        var innerLeft = $scope.slide.hSliderInL * 16;
+        var innerTop = $scope.slide.vSliderInL * 12;
+        var innerWidth = ($scope.slide.hSliderInR
+                       - $scope.slide.hSliderInL) * 16;
+        var innerHeight = ($scope.slide.vSliderInR
+                        - $scope.slide.vSliderInL) * 12;
+        webBox.css('left', outerLeft + 'px');
+        webBox.css('top', outerTop + 'px');
+        webBox.css('width', outerWidth + 'px');
+        webBox.css('height', outerHeight + 'px');
+
+        if (outerLeft > 0) {
+            outerWidth += outerLeft;
+            outerLeft = 0;
+        }
+        outerWidth = Math.max(outerWidth, 1600);
+        if (outerTop > 0) {
+            outerHeight += outerTop;
+            outerTop = 0;
+        }
+        outerHeight = Math.max(outerHeight, 1200);
+
+        console.log(outerLeft,outerTop);
+
+        blocker.north.css('left', outerLeft + 'px');
+        blocker.north.css('top', outerTop + 'px');
+        blocker.north.css('width', outerWidth + 'px');
+        blocker.north.css('height', innerTop - outerTop + 'px');
+        blocker.south.css('left', outerLeft + 'px');
+        blocker.south.css('top', innerTop + innerHeight + 'px');
+        blocker.south.css('width', outerWidth + 'px');
+        blocker.south.css('height', outerTop + outerHeight
+                                  - innerTop - innerHeight + 'px');
+        blocker.west.css('left', outerLeft + 'px');
+        blocker.west.css('top', innerTop + 'px');
+        blocker.west.css('width', innerLeft - outerLeft + 'px');
+        blocker.west.css('height', innerHeight + 'px');
+        blocker.east.css('left', innerLeft + innerWidth + 'px');
+        blocker.east.css('top', innerTop + 'px');
+        blocker.east.css('width', outerLeft + outerWidth
+                                - innerLeft - innerWidth + 'px');
+        blocker.east.css('height', innerHeight + 'px');
+
+        $scope.slide.zoom = Number($scope.slide.zoom) || 0;
+        var zoom = Math.exp($scope.slide.zoom * 0.1);
+        console.log(zoom);
+        web.css('width', (100/zoom) + '%');
+        web.css('height', (100/zoom) + '%');
+        web.css('-ms-zoom', zoom);
+        web.css('-moz-transform', 'scale(' + zoom + ')');
+        web.css('-webkit-transform', 'scale(' + zoom + ')');
+    });
+    
+    $scope.zoomIn = function() { $scope.slide.zoom ++; };
+    $scope.zoomOut = function() { $scope.slide.zoom --; };
 }).
 directive('ngRangeSlider', function() {
     return {
         restrict: "A",
+        scope: {
+            ngFrom: '=',
+            ngTo: '='
+        },
         link: function(scope, element, attrs) {
-            $(element).noUiSlider({
-                start: [0, 100], connect: true,
+            var slider = $(element);
+            slider.noUiSlider({
+                start: [scope.ngStart || 0, scope.ngTo || 100],
+                connect: true,
                 orientation: attrs.orientation,
                 range: { 'min': Number(attrs.min),
                          'max': Number(attrs.max) }
             });
-            $(element).css('display', 'inline-block');
+            slider.css('display', 'inline-block');
+            slider.css('margin', '15px');
+            slider.on('change', function() {
+                var val = slider.val();
+                scope.$apply(function(){
+                    scope.ngFrom = Number(val[0]);
+                    scope.ngTo = Number(val[1]);
+                });
+            });
+            scope.$watch('ngFrom', function(newFrom) {
+                if (newFrom == undefined) {
+                    scope.ngFrom = 0;
+                }
+                slider.val([newFrom, null]);
+            });
+            scope.$watch('ngTo', function(newTo) {
+                if (newTo == undefined) {
+                    scope.ngTo = 100;
+                }
+                slider.val([null, newTo]);
+            });
         }
     };
 });

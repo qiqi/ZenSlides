c2401969a30dc9642481df3f4a356980e590b320
Qiqi Wang <qiqi@mit.edu>
2014-12-01 11:58:11 -0500

    Redirect not working?

--- js/auth.js
+++ js/auth.js
@@ -7,16 +7,16 @@ function checkAuth() {
     var auth = firebase.root.getAuth();
     if (auth) {
         firebase.ref = firebase.root.child(auth.uid);
-        $(document).ready( function() {
-            window.location.replace('index-chooser.html');
-        });
+        window.location.replace('index-chooser.html');
     }
 }
 
-if ($('#login-button').length) {
-    // login screen
-    checkAuth();
-}
+$(document).ready( function()) {
+    if ($('#login-button').length) {
+        // login screen
+        checkAuth();
+    }
+});
 
 function firebaseLogin() {
     firebase.root.authWithOAuthRedirect('github', function(err) {

4846656aaf65cd6f9db2524ffe6d3aab5b3fac80
Qiqi Wang <qiqi@mit.edu>
2014-12-01 13:28:01 -0500

    license in files

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,19 @@
+//. This file is part of ZenSlides, the web tool for preparing a ZenBoard presentation.
+//. Copyright (C) 2014 Qiqi Wang
+//. 
+//. This program is free software: you can redistribute it and/or modify
+//. it under the terms of the GNU Affero General Public License as published by
+//. the Free Software Foundation, either version 3 of the License, or
+//. (at your option) any later version.
+//. 
+//. This program is distributed in the hope that it will be useful,
+//. but WITHOUT ANY WARRANTY; without even the implied warranty of
+//. MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//. GNU Affero General Public License for more details.
+//. 
+//. You should have received a copy of the GNU Affero General Public License
+//. along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Firebase.INTERNAL.forceWebSockets();
 var firebase = {
     root: new Firebase("https://zenboard.firebaseio.com/private")

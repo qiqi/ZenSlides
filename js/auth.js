619958530195e744fc422971cfdc41b0d82ab110
Qiqi Wang <qiqi.wang@gmail.com>
2014-12-01 22:25:40 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -40,6 +40,12 @@ function firebaseLogin() {
     });
 }
 
+function firebaseLogout() {
+    if (firebase.ref) {
+        firebase.ref.unauth();
+    }
+}
+
 function GUID() {
     function S4() {
         return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
